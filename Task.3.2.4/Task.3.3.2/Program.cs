37dee9835dfa43257c95379bec7980a52976633b
Maksimenkova Olga <blackviolet@mail.ru>
2025-01-28 09:40:53 +0300

    Task._3._3._2 from lesson has added

---
+++ Task.3.2.4/Task.3.3.2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задание 3.3.2
+//Залейте в Git программу, которую мы рассмотрели выше. Она вам ещё пригодится.
+namespace Task._3._3._2
+{
+   class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            string MyName = "Jane";
+            byte MyAge = 27;
+            bool HaveIApet = true;
+            double MyShoeSize = 37.5;
+
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("MyAge " + MyAge);
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
+        }
+    }
+}
